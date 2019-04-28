36a80583eb30cc4716b19f00463a77d8387b07d3
Lenk <jmvg.comp@gmail.com>
2019-04-28 02:07:38 -0300

    Conclusão do pequeno projeto com threads

--- src/br/com/fnr/tap/Threads/view/Program.java
+++ src/br/com/fnr/tap/Threads/view/Program.java
@@ -12,23 +12,10 @@ public class Program {
 
         Controlador<Pacotes> pacotesControlador = new Controlador<>();
         Thread t1 = new Thread(new Correios(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        Thread t2 = new Thread(new Correios(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        Thread t3 = new Thread(new Correios(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        Thread t4 = new Thread(new Correios2(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        Thread t5 = new Thread(new Correios2(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        Thread t6 = new Thread(new Correios2(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
-        try {
-            t1.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Thread t2 = new Thread(new Correios2(pacotesControlador,10, 10), String.valueOf(TipoEntrega.randomEntrega()));
+
         t1.start();
         t2.start();
-        t3.start();
-        t4.start();
-        t5.start();
-        t6.start();
-
 
         for(int i=0; i<10; i++){
             pacotesControlador.add(Pacotes.randomSERVICE());
